f6160b0d844f2c52860f9f7739e1bb165a9b82b1
ZhangYantao <zhangyantao@rivergame.net>
2022-05-25 15:52:48 +0800

    perf: 尝试git commit

--- .husky/common.sh
+++ .husky/common.sh
@@ -1,17 +1,9 @@
-#!/bin/sh
-###
- # @Description  : 
- # @Author       : Zhang Yantao
- # @Date         : 2022-05-24 23:35:25
- # @LastEditors  : Zhang Yantao
- # @LastEditTime : 2022-05-25 11:21:06
- # @FilePath     : /.husky/common.sh
-### 
-# command_exists () {
-#   command -v "$1" >/dev/null 2>&1
-# }
+#!/bin/bash
+command_exists () {
+  command -v "$1" >/dev/null 2>&1
+}
 
-# # Workaround for Windows 10, Git Bash and Yarn
-# if command_exists winpty && test -t 1; then
-#   exec < /dev/tty
-# fi
+# Workaround for Windows 10, Git Bash and Yarn
+if command_exists winpty && test -t 1; then
+  exec < /dev/tty
+fi
